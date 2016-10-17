1d82e66ccc8bdbafa16196a8205205b6726c0ad6
GSSL <252014525@qq.com>
2016-10-17 23:01:29 +0800

    second
    add webpack-dev-server

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 /**
  * Created by root on 16-10-17.
  */
+// require('es5-shim');
+require("es5-shim-sham");
+require('console-polyfill');
 import React from "react";
 import  ReactDOM from "react-dom";
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,19 @@ module.exports = {
         path: "./bin",
         filename: "ie8Support.js"
     },
+    debug: true,
     module: {
         loaders: [
             {
                 test: /\.jsx?$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/
+            }
+        ],
+        postLoaders: [
+            {
+                test: /\.js$/,
+                loaders: ['es3ify-loader']
             }
         ]
     }
